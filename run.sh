5b755541e24181dd14c603c60fe30a5121d9419c
Leon Grünewald <leon.gruenewald@kreativrudel.de>
2020-08-12 14:19:37 +0200

    Edit shell to allow routes, better shell handling and add gitignore

--- run.sh
+++ run.sh
@@ -1,13 +1,17 @@
-#!/bin/bash
-
-args="$@"
+#!/usr/bin/env bash
 
 args="$@ -p 80"
 
 file=/data/db.json
 if [ -f $file ]; then
     echo "Found db.json, trying to open"
-    args="$args db.json"
+    args="$args -w db.json"
+fi
+
+file=/data/routes.json
+if [ -f $file ]; then
+    echo "Found routes.js file, trying to open"
+    args="$args -r routes.json"
 fi
 
 file=/data/file.js
